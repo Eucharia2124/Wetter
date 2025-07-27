2aec30e228e2f60fcba5c3e7ce15b442ec1aeb80
Eucharia2124 <obiageristoehr2124@gmail.com>
2025-07-27 20:10:35 +0200

    apiUrl & key added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+function refreshWeather(response) {
+    console.log(response.data);
+}
+function searchCity(city) {
+    let apiKey = "35f4a664892ct08f69ca41oe92eb5099";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}&units=metric`;
+    axios.get(apiUrl).then(refreshWeather);
+}
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
