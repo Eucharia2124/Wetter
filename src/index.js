78ac8b78b2cb276f7230a9d728861d595bb19f3e
Eucharia2124 <obiageristoehr2124@gmail.com>
2025-07-28 21:06:48 +0200

    added search element

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
+
 function refreshWeather(response) {
-    console.log(response.data);
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
+
 }
+
 function searchCity(city) {
     let apiKey = "35f4a664892ct08f69ca41oe92eb5099";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}&units=metric`;
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
     axios.get(apiUrl).then(refreshWeather);
 }
+
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-   let cityElement = document.querySelector("#city");
-   cityElement.innerHTML = searchInput.value; 
+    
+    searchCity(searchInput.value); // Call searchCity function to fetch weather data
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+
+
+ 
