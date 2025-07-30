224ecc6f0e398554c9d5b820f89cdce1fde3610f
Eucharia2124 <obiageristoehr2124@gmail.com>
2025-07-30 19:59:23 +0200

    injected more weather data

--- src/index.js
+++ src/index.js
@@ -3,10 +3,40 @@ function refreshWeather(response) {
     let temperatureElement = document.querySelector("#temperature");
     let temperature = response.data.temperature.current;
     let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windSpeedElement = document.querySelector("#wind-speed");
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time * 1000);
+
+    console.log(response.data);
 
     cityElement.innerHTML = response.data.city;
-    temperatureElement.innerHTML = Math.round(temperature);
 
+    timeElement.innerHTML = formatDate(date); 
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windSpeedElement.innerHTML = `${Math.round(response.data.wind.speed)}km/h`;
+    temperatureElement.innerHTML = Math.round(temperature);
+}
+function formatDate(date) {
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let days = [
+        "Sunday", 
+        "Monday",
+         "Tuesday", 
+         "Wednesday",
+          "Thursday", 
+          "Friday", 
+          "Saturday"
+        ];
+        let day = days[date.getDay()];
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+       
+    return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
@@ -19,12 +49,13 @@ function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
     
-    searchCity(searchInput.value); // Call searchCity function to fetch weather data
+    searchCity(searchInput.value); 
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
+searchCity("Paris"); 
 
 
  
